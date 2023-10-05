46e98545d1abd05b78835b45f0ea906a543a1eb7
kashifkhan099 <139098480+kashifkhan099@users.noreply.github.com>
2023-10-05 11:23:47 +0500

    Update App.js

--- App.js
+++ App.js
@@ -28,3 +28,55 @@ console.log(result);
 // var result = number.toFixed(2);
 
 // console.log(result);
+ var date = new Date();
+// var day = date.getDay();
+// var array = [
+//   "Sunday",
+//   "Monday",
+//   "Tuesday",
+//   "Wednesday",
+//   "Thursday",
+//   "Friday",
+//   "Saturday",
+// ];
+
+// console.log(array[day]);
+
+// var month = date.getMonth();
+
+// var arr = [
+//   "Jan",
+//   "Feb",
+//   "Mar",
+//   "Apr",
+//   "May",
+//   "Jun",
+//   "July",
+//   "Aug",
+//   "Sep",
+//   "Oct",
+//   "Nov",
+//   "Dec",
+// ];
+// console.log(arr[month]);
+
+// console.log(date.getDate());
+// console.log(date.getFullYear());
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+// console.log(date.getSeconds());
+// console.log(date.getMilliseconds());
+
+// console.log(date.getTime());
+
+// var doomsDay = new Date("June 30, 2035");
+// var date = new Date();
+
+// var calculatedTime = doomsDay.getTime() - date.getTime();
+
+// var result = calculatedTime / (1000 * 60 * 60 * 24);
+// console.log(Math.floor(result));
+
+// var doomsDay = new Date("2035 01 12:02");
+
+// console.log(doomsDay);
