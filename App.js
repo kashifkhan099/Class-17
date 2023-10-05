33715392f9fa6948fe1336ee38547a97488638bf
kashifkhan099 <139098480+kashifkhan099@users.noreply.github.com>
2023-10-05 10:11:35 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+// var result = Math.random() * 6;
+
+// var result2 = Math.floor(result) + 1;
+// console.log(result2);
+
+// var number = +prompt("Enter Number Here");
+
+// if (result2 === number) {
+//   alert("Your number is matched");
+// }
+
+// var result = "12" - "Duck";
+
+// console.log(result);
+
+var result1 = parseInt("12.557676676 Duck");
+var result2 = parseFloat("12.557676676 Years");
+
+var result = Number("12 Years")
+var result = parseInt(true);
+console.log(result);
+
+// var number = 12;
+// var result = number.toString();
+// console.log(result);
+
+// var number = 12.5636565656565;
+// var result = number.toFixed(2);
+
+// console.log(result);
